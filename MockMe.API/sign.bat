2f995526848ca9cc0fed03c8edfd2addf205f14b
Solly Fathi <sfathi@qic.com>
2024-02-04 20:09:53 +1000

    Sign all dll files in unit-test poject folder

--- MockMe.API/sign.bat
+++ MockMe.API/sign.bat
@@ -8,4 +8,5 @@ signtool sign /a /v "%1Swashbuckle.AspNetCore.Swagger.dll"
 signtool sign /a /v "%1Swashbuckle.AspNetCore.SwaggerGen.dll"
 signtool sign /a /v "%1Swashbuckle.AspNetCore.SwaggerUI.dll"
 signtool sign /a /v "%1Newtonsoft.Json.dll"
+signtool sign /a /v "%1AutoMapper.dll"
 echo Signing files success.
